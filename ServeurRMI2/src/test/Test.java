d9146c29905474631b4b6da608768c9e5b2cd53a
BrightProgrammer7 <e-akhmim.a@ucd.ma>
2024-01-26 19:27:34 +0000

    RMI

---
+++ ServeurRMI2/src/test/Test.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author HP
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        try {
+//           IDao<Machine> dao = new MachineService();
+//                   dao.create(new Machine("latitude", "DELL", 3000));
+//        dao.create(new Machine("thinkpad", "Lenovo", 2000));
+//        dao.create(new Machine("elitebook", "HP", 4000));
+//        
+//        for(Machine m: dao.findAll()){
+//            System.out.println(m);
+//        }
+//        } catch (RemoteException ex) {
+//            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+// 
+
+    }
+}
